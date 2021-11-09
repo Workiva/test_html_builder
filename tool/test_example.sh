1b32b921418f048ebd373f8bd8962367fc72cf8c
evanweible-wf <evan.weible@workiva.com>
2021-11-09 13:47:24 -0700

    Update example consumer tests to exercise `--build-filter` use case

--- tool/test_example.sh
+++ tool/test_example.sh
@@ -7,3 +7,8 @@ dart pub get
 dart run build_runner build --delete-conflicting-outputs
 dart run build_runner test
 dart run test_html_builder:browser_aggregate_tests
+
+# These commands target a scenario where there is no build cache and someone
+# uses build filters to run a subset of tests.
+dart run build_runner clean
+dart run build_runner test --delete-conflicting-outputs --build-filter="test/unit/css_test.**" -- test/unit/css_test.dart
