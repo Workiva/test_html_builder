068b4f7958191eccb2e11cd6851a500337e6c151
evanweible-wf <evan.weible@workiva.com>
2024-11-12 09:59:18 -0700

    Ensure a `pub upgrade` when testing example project

--- tool/test_example.sh
+++ tool/test_example.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd example/project
-dart pub get
+dart pub upgrade
 dart run build_runner build --delete-conflicting-outputs
 dart run build_runner test
 dart run test_html_builder:browser_aggregate_tests
