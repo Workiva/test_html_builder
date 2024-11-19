6934175425d253b7704ea069b93de0bc27aa3607
evanweible-wf <evan.weible@workiva.com>
2024-11-19 07:40:52 -0700

    Workaround https://github.com/dart-lang/test/issues/2294

--- tool/test_example.sh
+++ tool/test_example.sh
@@ -6,7 +6,7 @@ cd example/project
 dart pub upgrade
 dart run build_runner build --delete-conflicting-outputs
 dart run build_runner test -- --concurrency=1 # concurrency=1 is a workaround for this issue: https://github.com/dart-lang/test/issues/2294
-dart run test_html_builder:browser_aggregate_tests
+dart run test_html_builder:browser_aggregate_tests --test-args="--concurrency=1" # concurrency=1 is a workaround for this issue: https://github.com/dart-lang/test/issues/2294
 
 # These commands target a scenario where there is no build cache and someone
 # uses build filters to run a subset of tests.
